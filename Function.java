d9e04a4026fe4f390713caa192f480f032877fd1
mikamp116 <mikamardanyan@gmail.com>
2019-05-19 10:52:21 +0200

    Clase Function

---
+++ Function.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Function {
+
+	public ArrayList<VarList> varLists;
+	public String type;
+	public String name;
+
+	public Function() {
+		this.varLists = new ArrayList<>();
+	}
+
+	public StringBuilder print() {
+		StringBuilder r = new StringBuilder();
+		for (VarList vl : this.varLists) {
+			for (String v : vl.vars) {
+				r.append(vl.tipo).append(" ").append(v).append(", ")
+			}
+		}
+		r.setLength(r.length()-2);
+		return r;
+	}
+}
